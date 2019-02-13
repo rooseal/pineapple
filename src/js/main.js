05ef76656fd2b069709e4e9f7f1aa1cee58d5e95
Alyssa Roose <roose.alyssa@gmail.com>
2019-02-13 19:48:05 +0100

    Setup javascript state management

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,71 @@
-console.log('Houston, we have javascript');
+(function () {
+  // START - General functions
+  function createDispatcher (reducer, store, emitter) {
+    return function dispatch (action) {
+            // Make sure we have a valid action object
+      action = action || { type: 'PASS_THROUGH' };
+      if (action.type === undefined) {
+        action.type = 'PASS_THROUGH';
+      }
+
+      // Update the store
+      store = reducer(store, action);
+
+      // Emit event
+      emitter.emitEvent(action.type, [store]); 
+    }
+  }
+  // END - General functions
+
+  // Define site state
+  const state = {
+    menu: false
+  };
+
+  // Define user actions
+  const actions = {
+    openMenu () {
+      return {
+        type: 'OPEN_MENU'
+      }
+    }
+  }
+
+  // Define state reducer function
+  function reducer (state, action) {
+    switch (action.type) {
+      case 'OPEN_MENU':
+        return {
+          ...state,
+          menu: true
+        }
+      default:
+        return state;
+    }
+  }
+
+  // Define functions to update the view
+  function handleMenu(visibility) {
+    // Execute code to show menu
+    console.log('Setting menu visibility to ', visibility);
+  }
+
+
+  //
+  // Run code when window loads
+  //
+
+  console.log('Houston, we have javascript');
+
+  // Create state dispatcher
+  const emitter = new EventEmitter();
+  const dispatch = createDispatcher(reducer, state, emitter);
+  
+  // Bind user interactions to actions
+  document.getElementById('menuToggle').addEventListener('click', function () { dispatch(actions.openMenu()) });
+
+  // Bind actions to behaviour
+  emitter.addListener('OPEN_MENU', function (state) {
+    handleMenu(state.menu);
+  });
+}());
