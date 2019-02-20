881932743130fc8b1c195f5ea631877d6eb04c95
Alyssa Roose <roose.alyssa@gmail.com>
2019-02-20 07:31:35 +0100

    Add scroll animation + footer animation + fix to store dispatcher

--- src/js/main.js
+++ src/js/main.js
@@ -18,11 +18,29 @@
           index: index || 0
         }
       }
+    },
+    showFooter () {
+      return {
+        type: 'SHOW_FOOTER'
+      }
+    },
+    hideFooter () {
+      return {
+        type: 'HIDE_FOOTER'
+      }
     }
   }
 
+  // Define site state
+  let state = {
+    menu: false,
+    footer: false,
+    pages: Array.from(document.querySelectorAll('.page') || []),
+    currentPage: 0,
+  };
+
   // START - General functions
-  function createDispatcher (reducer, store, emitter) {
+  function createDispatcher (reducer, emitter) {
     return function dispatch (action) {
             // Make sure we have a valid action object
       action = action || { type: 'PASS_THROUGH' };
@@ -31,10 +49,10 @@
       }
 
       // Update the store
-      store = reducer(store, action);
+      state = reducer(state, action);
 
       // Emit event
-      emitter.emitEvent(action.type, [store]); 
+      emitter.emitEvent(action.type, [state]); 
     }
   }
 
@@ -62,14 +80,34 @@
     indexContainer.innerHTML = '';
     indexContainer.appendChild(fragment);
   }
-  // END - General functions
 
-  // Define site state
-  const state = {
-    menu: false,
-    pages: Array.from(document.querySelectorAll('.page') || []),
-    currentPage: 0,
-  };
+  function throttleAction (speed) {
+    let state = {
+      action: undefined,
+      handle: undefined
+    }
+
+    speed = speed || 200; // Set default throttle speed
+
+    return function throttler (fn) {
+      state.action = fn;
+
+      if (state.handle === undefined) {
+
+        state.handle = setTimeout(function () {
+          clearInterval(state.handle);
+          state.handle = undefined;
+        }, speed);
+
+        state.action();
+        state.action = undefined;
+
+        console.log({ handle: state.handle });
+        console.log('Create new timeout')
+      }
+    }
+  }
+  // END - General functions
 
   // Define state reducer function
   function reducer (state, action) {
@@ -89,6 +127,16 @@
           ...state,
           currentPage: action.payload.index
         }
+      case 'SHOW_FOOTER':
+        return {
+          ...state,
+          footer: true
+        }
+      case 'HIDE_FOOTER':
+        return {
+          ...state,
+          footer: false
+        }
       default:
         return state;
     }
@@ -102,10 +150,36 @@
   }
 
   function handleUpdatePage (index, pages) {
-    console.log('Let\'s go to page ', pages[index]);
+    // console.log('Let\'s go to page ', pages[index]);
     
+    let speed = 100;
+    let direction;
+
+    let target = pages[index].offsetTop;
+
+    // console.log('TARGET = ' + target);
+
     // Update scroll position
-    window.scrollTo(0, pages[index].offsetTop);
+    (function animatedScroll (source) {
+      requestAnimationFrame(function () {
+        if (source < target) {
+          direction = 'down';
+        } else {
+          direction = 'up';
+        }
+  
+        const newPosition = direction === 'down' ? source + speed : source - speed;
+        // console.log('Scroll from ' + source + ' ' + direction + ' to ' + newPosition  + ' at ' + speed + ' speed');
+  
+        if ((direction === 'down' && newPosition >= target) || (direction === 'up' && newPosition <= target)) {
+          window.scrollTo(0, target);
+          // console.log('We are there');
+        } else {
+          window.scrollTo(0, newPosition);
+          animatedScroll(newPosition);
+        }
+      })
+    })(window.scrollY);
 
     // Update active page index
     document.querySelectorAll('.index__number').forEach(function (currentElement, currentIndex) {
@@ -117,17 +191,50 @@
     });
 
     // Update arrow controls
-    if (index <= 0) {
-      document.getElementById('previousPage').style.display = 'none';
-    } else {
-      document.getElementById('previousPage').style.display = 'block';
-    }
+    // if (index <= 0) {
+    //   document.getElementById('previousPage').style.display = 'none';
+    // } else {
+    //   document.getElementById('previousPage').style.display = 'block';
+    // }
 
-    if (index >= pages.length - 1) {
-      document.getElementById('nextPage').style.display = 'none';
-    } else {
-      document.getElementById('nextPage').style.display = 'block';
-    }
+    // if (index >= pages.length - 1) {
+    //   document.getElementById('nextPage').style.display = 'none';
+    // } else {
+    //   document.getElementById('nextPage').style.display = 'block';
+    // }
+  }
+
+  function handleFooter (showFooter) {
+    // document.querySelector('footer').style.bottom = showFooter ? '0' : '100%';
+    let speed = 100;
+    let direction;
+
+    let element = document.querySelector('footer');
+    let target = showFooter ? element.offsetHeight : 0;
+
+    console.log({ element });
+
+    // Update scroll position
+    (function animatedScroll (source) {
+      requestAnimationFrame(function () {
+        if (source < target) {
+          direction = 'down';
+        } else {
+          direction = 'up';
+        }
+  
+        const newPosition = direction === 'down' ? source + speed : source - speed;
+        console.log('Scroll from ' + source + ' ' + direction + ' to ' + newPosition  + ' at ' + speed + ' speed');
+  
+        if ((direction === 'down' && newPosition >= target) || (direction === 'up' && newPosition <= target)) {
+          element.style.marginTop = '-' + target + 'px';
+          // console.log('We are there');
+        } else {
+          element.style.marginTop = '-' + newPosition + 'px';
+          animatedScroll(newPosition);
+        }
+      })
+    })(showFooter ? 0 : element.offsetHeight);
   }
 
 
@@ -139,15 +246,54 @@
 
   // Create state dispatcher
   const emitter = new EventEmitter();
-  const dispatch = createDispatcher(reducer, state, emitter);
+  const dispatch = createDispatcher(reducer, emitter);
   
   buildPageIndices(state.pages, dispatch);
 
   // Bind user interactions to actions
   document.getElementById('menuToggle').addEventListener('click', function () { dispatch(actions.openMenu()) });
   document.getElementById('menuClose').addEventListener('click', function () { dispatch(actions.closeMenu()) });
-  document.getElementById('previousPage').addEventListener('click', function () { dispatch(actions.goToPage(Math.max(state.currentPage - 1, 0))) });
-  document.getElementById('nextPage').addEventListener('click', function () { dispatch(actions.goToPage(Math.min(state.currentPage + 1, state.pages.length - 1))) });
+  document.getElementById('previousPage').addEventListener('click', function () {
+    if (state.footer) {
+      dispatch(actions.hideFooter());
+    } else {
+      dispatch(actions.goToPage(Math.max(state.currentPage - 1, 0)));
+    }
+  });
+  document.getElementById('nextPage').addEventListener('click', function () {
+    console.log({ state })
+    if (state.currentPage + 1 >= state.pages.length) {
+      dispatch(actions.showFooter());
+    } else {
+      dispatch(actions.goToPage(state.currentPage + 1));
+    }
+  });
+  
+  // Add custom scroll behviour
+  const throttleScroll = throttleAction(1000);
+  window.addEventListener('wheel', function (event) {
+    event.preventDefault();
+
+    if (!state.menu) {
+      throttleScroll(function () {
+        // console.log('Scrolled ', event.wheelDeltaY > 0 ? 'up' : 'down')
+        if (event.wheelDeltaY > 0) {
+          if (state.footer) {
+            dispatch(actions.hideFooter());
+          } else {
+            dispatch(actions.goToPage(Math.max(state.currentPage - 1, 0)));
+          }
+        } else {
+          // console.log('Scroll down from ' + state.currentPage + ' in ' + state.pages.length)
+          if (state.currentPage + 1 >= state.pages.length) {
+            dispatch(actions.showFooter());
+          } else {
+            dispatch(actions.goToPage(state.currentPage + 1));
+          }
+        }
+      });
+    }
+  });
 
   // Bind actions to behaviour
   emitter.addListener('OPEN_MENU', function (state) {
@@ -156,7 +302,13 @@
   emitter.addListener('CLOSE_MENU', function (state) {
     handleMenu(state.menu);
   });
+  emitter.addListener('SHOW_FOOTER', function (state) {
+    handleFooter(state.footer);
+  });
+  emitter.addListener('HIDE_FOOTER', function (state) {
+    handleFooter(state.footer);
+  });
   emitter.addListener('CHANGE_PAGE', function (state) {
     handleUpdatePage(state.currentPage, state.pages);
-  })
+  });
 }());
